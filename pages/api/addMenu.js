6f95b6592d22afbb42e979d6476a7bde77418a0b
kumarprashant24 <kumar.prashant@tothenew.com>
2022-06-28 15:51:37 +0530

    updated package

--- pages/api/addMenu.js
+++ pages/api/addMenu.js
@@ -4,10 +4,9 @@ import restaurant from '../../models/resturant.model'
 conn();
 
 export default async function resturant(req, res) {
-    const data = (req.body);
+   
     const menu = {food_name:req.body.food,price:req.body.price,image:req.body.picture}
     const restro_id =  mongoose.Types.ObjectId(req.body.restro_id);
-
     const result = await restaurant.findById(restro_id);
     result.menu.push(menu);
     result.save();
