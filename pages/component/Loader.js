69a1accb4a437fed21102d758740b78fdb0e179c
kumarprashant24 <kumar.prashant@tothenew.com>
2022-06-30 13:31:05 +0530

    changes in loader

--- pages/component/Loader.js
+++ pages/component/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loader() {
   return (
-    <div class="sticky-load">
+    <div className="sticky-load">
       <div className='loader'></div>
     </div>
   )
