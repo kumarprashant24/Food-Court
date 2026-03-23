efec9d294d688947b1e841f547c93122a1aefe2f
Kumar Prashant <kumar.prashant@tothenew.com>
2026-03-23 21:02:45 +0530

    add log for debugging

--- pages/component/Login.js
+++ pages/component/Login.js
@@ -11,8 +11,12 @@ export default function Login({ toggle }) {
 
 
                     </div>
-                    <div className='d-flex  justify-content-center ' style={{cursor:"pointer"}}>
-                        <div className='bg-white d-flex p-2 rounded' onClick={() => signIn(toggle.google.id)}>
+                    <div className='d-flex  justify-content-center ' style={{ cursor: "pointer" }}>
+                        <div className='bg-white d-flex p-2 rounded' onClick={() => {
+                            console.log('toggle.google',toggle.google);
+                            
+                            signIn(toggle.google.id)
+                            }}>
                             <img className='google' src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png' />
                             <div className='d-flex align-items-center' style={{ color: 'rgb(105, 105, 105)' }}>Continue With Google</div>
                         </div>
